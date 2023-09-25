d5a98975532410a169db29219d0a9ca8868d85cc
Isaev Ivan <valamart9@gmail.com>
2023-09-25 08:27:31 +0300

    dev:creatingAnInput

---
+++ fastpay-app/src/components/mainComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h2>i'm main component</h2>
+        <main-input-vue v-for="item in inputsArr" 
+        :key=item.id 
+        :title="item.title" 
+        :type="item.type"
+        :plaseholder="item.placeholder"></main-input-vue>
+    </div>
+</template>
+
+<script lang="ts">
+import mainInputVue from '../serviceComponents/mainInput.vue'
+
+    export default {
+        type:Array,
+
+        components: {
+            mainInputVue
+        },
+        data(){
+            return {
+                inputsArr: [
+                { id: 1, title: 'input1', type: 'text', placeholder: 'input1' },
+                { id: 2, title: 'input2', type: 'text', placeholder: 'input2' },
+                { id: 3, title: 'input3', type: 'text', placeholder: 'input3' },
+              ]
+            }
+            
+        }
+    }       
+
+</script>
---
+++ fastpay-app/src/serviceComponents/mainInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+       
+        <label for="">{{ title }}</label>
+        <input type="text" placeholder="input">
+    </div>
+   
+</template>
+
+<script lang="ts">
+    export default {
+        props:['id', 'title', 'type', 'placeholder'],
+        data(){
+            return {
+            
+            }
+            
+        }
+    }
+</script>
+
+<style scoped>
+input {
+    color: black
+}
+</style>
